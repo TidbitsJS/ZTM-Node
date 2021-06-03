034422c6ef80b7cb72da2914e1807f47f6d103aa
Tidbits JS <tidbitsjs@gmail.com>
2021-06-03 18:02:46 +0530

    asynchronous event

--- Internals/event.js
+++ Internals/event.js
@@ -6,7 +6,7 @@ celebrity.on("race win", function () {
   console.log("Congratulations! You are the best!");
 });
 
-//Many events sharing same name can be registered.
+// Many events sharing same name can be registered.
 // Subscribe to celebrity for Observer 2
 celebrity.on("race win", () => {
   console.log("Boo I could have done better than that!");
@@ -41,7 +41,30 @@ challenge.on("hack", (result) => {
   }
 });
 
-// Same as above excpet passing arguments, "hacked" or "failed" in this case.
+// Same as above except passing arguments, "hacked" or "failed" in this case.
 challenge.emit("hack", "hacked");
 challenge.emit("hack", "failed");
 challenge.emit("hack", "hacked");
+
+// EventEmitters calls all listeners synchoronously in the order in which they registered.
+// Listener function can switch to an asynchronous mode of operation using -
+//      1. setImmediate()
+//      2. process.nextTick()
+
+const asyncEmitter = new EventEmitter();
+asyncEmitter.on("async-event", (a, b) => {
+  console.log("Before async");
+  setImmediate(() => {
+    console.log("this happens asynchronously");
+  });
+
+  console.log("After setImmediate");
+
+  setTimeout(() => {
+    console.log(`a is ${a} & b is ${b}`);
+  }, 3000);
+
+  console.log("End async");
+});
+
+asyncEmitter.emit("async-event", "a", "b");
