1ebaf73f265b204fb3b0bd771528f5bff60359af
Tidbits JS <tidbitsjs@gmail.com>
2021-06-07 23:23:36 +0530

    send file

--- Express/controllers/messages.controller.js
+++ Express/controllers/messages.controller.js
@@ -1,8 +1,19 @@
+const path = require("path");
+
 function getMessages(req, res) {
-  // Content-Type will be set to 'text/html'
-  res.send(
-    "<ul><li>Hello, how are you?</li><li>Will you be my friend?</li></ul>"
-  );
+  /*
+    // Content-Type will be set to 'text/html'
+    res.send(
+        "<ul><li>Hello, how are you?</li><li>Will you be my friend?</li></ul>"
+    );
+
+  */
+
+  // path.join() is to help with all path scenario on different machine i.e., on linux /folder/files.jpg & windows \folder\files.jpg
+
+  // __dirname is to get the name of the folder the current file lies in, here, dirname - controllers
+  const sendFile = path.join(__dirname, "..", "public", "skimountain.jpg");
+  res.sendFile(sendFile);
 }
 
 function postMessages(req, res) {
