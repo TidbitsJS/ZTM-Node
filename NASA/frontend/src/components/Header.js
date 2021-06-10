e2430e1f3339db2807025db194c74cc382a3f8b4
Tidbits JS <tidbitsjs@gmail.com>
2021-06-10 22:44:13 +0530

    customizing header

--- NASA/frontend/src/components/Header.js
+++ NASA/frontend/src/components/Header.js
@@ -13,11 +13,17 @@ const styles = (theme) => ({
   root: {
     display: "flex",
     flexDirection: "row",
+    maxWidth: "90%",
     lineHeight: "80px",
+    justifyContent: "space-between",
   },
   logo: {
     display: "inherit",
     marginTop: "15px",
+  },
+  brand: {
+    display: "flex",
+    flexDirection: "row",
   },
   nav: {
     display: "inherit",
@@ -27,6 +33,12 @@ const styles = (theme) => ({
     fontWeight: "bold",
     marginLeft: "10px",
     marginRight: "15px",
+    fontSize: 28,
+  },
+  shortBanner: {
+    display: "none",
+    fontWeight: "bold",
+    marginLeft: "10px",
     fontSize: 28,
   },
   clickable: {
@@ -47,11 +59,11 @@ const styles = (theme) => ({
     logo: {
       display: "none",
     },
-    img: {
-      display: "none",
-    },
     banner: {
       display: "none",
+    },
+    shortBanner: {
+      display: "inherit",
     },
     button: {
       padding: [0, 8],
@@ -67,39 +79,48 @@ const Header = (props) => {
   return (
     <ArwesHeader animate>
       <Centered className={classes.root} {...rest}>
-        <img
-          src="/favicon.png"
-          alt=""
-          className={classes.img}
-          style={{
-            margin: "15px 10px 15px 0",
-            height: "50px",
-            width: "auto",
-          }}
-        />
-        <Logo animate size={50} className={classes.logo} layer="header" />
-        <Words animate className={classes.banner}>
-          NASA Mission Control
-        </Words>
+        <div className={classes.brand}>
+          <img
+            src="/favicon.png"
+            alt=""
+            className={classes.img}
+            style={{
+              margin: "15px 10px 15px 0",
+              height: "50px",
+              width: "auto",
+            }}
+          />
+          <Logo animate size={50} className={classes.logo} layer="header" />
+          <Words animate className={classes.banner}>
+            NASA Mission Control
+          </Words>
+          <Words animate className={classes.shortBanner}>
+            NASA
+          </Words>
+        </div>
+
         <nav className={`${classes.nav}`}>
           <Clickable className={classes.clickable} onClick={onNav}>
             <Highlight className={classes.button} animate layer="header">
               <Link className={classes.link} to="/launch">
-                <i className="material-icons">check_circle_outline</i>Launch
+                {/* <i className="material-icons">check_circle_outline</i> */}
+                Launch
               </Link>
             </Highlight>
           </Clickable>
           <Clickable className={classes.clickable} onClick={onNav}>
             <Highlight className={classes.button} animate layer="header">
               <Link className={classes.link} to="/upcoming">
-                <i className="material-icons">update</i>Upcoming
+                {/* <i className="material-icons">update</i> */}
+                Upcoming
               </Link>
             </Highlight>
           </Clickable>
           <Clickable className={classes.clickable} onClick={onNav}>
             <Highlight className={classes.button} animate layer="header">
               <Link className={classes.link} to="/history">
-                <i className="material-icons">history</i>History
+                {/* <i className="material-icons">history</i> */}
+                History
               </Link>
             </Highlight>
           </Clickable>
