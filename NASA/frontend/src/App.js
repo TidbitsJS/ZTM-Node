7a9d2fd97944ae884ffa8a48116aad18324323dc
Tidbits JS <tidbitsjs@gmail.com>
2021-06-10 19:03:46 +0530

    creating NASA theme

--- NASA/frontend/src/App.js
+++ NASA/frontend/src/App.js
@@ -1,11 +1,20 @@
-import { ThemeProvider } from "arwes";
-import createTheme from "arwes/lib/tools/createTheme";
+import { Arwes, ThemeProvider, SoundsProvider, createTheme } from "arwes";
 import Footer from "./components/Footer";
+
+import { theme, resources, sounds } from "./settings";
 
 const App = () => {
   return (
-    <ThemeProvider theme={createTheme()}>
-      <Footer />
+    <ThemeProvider theme={createTheme(theme)}>
+      <SoundsProvider sounds={createTheme(sounds)}>
+        <Arwes
+          animate
+          background={resources.background.large}
+          pattern={resources.pattern}
+        >
+          {(anim) => <Footer />}
+        </Arwes>
+      </SoundsProvider>
     </ThemeProvider>
   );
 };
